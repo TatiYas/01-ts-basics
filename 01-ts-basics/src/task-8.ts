cc8fb1b5b4a3d9f9e4a7d9d13e8f584c14d4eda5
TatiYas <iasinovska_lawyer@ukr.net>
2025-10-26 18:10:02 +0100

    Update task-8.ts

--- 01-ts-basics/src/task-8.ts
+++ 01-ts-basics/src/task-8.ts
@@ -6,8 +6,8 @@ interface Post {
     body: string;
 }
 
-async function fetchPosts() {
-  const response = await axios.get(
+async function fetchPosts(): Promise <Post[]> {
+  const response = await axios.get <Post[]>(
     '<https://jsonplaceholder.typicode.com/posts>'
   );
   return response.data;
