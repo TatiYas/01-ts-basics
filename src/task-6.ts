ca062ca441d6b719e29dac4d31023373d3477af5
TatiYas <iasinovska_lawyer@ukr.net>
2025-10-29 14:30:05 +0100

    Update task-6.ts

--- src/task-6.ts
+++ src/task-6.ts
@@ -1,14 +1,9 @@
 function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
-const firstNumber = getFirstElement<number>([1, 2, 3]);        
-const firstString = getFirstElement<string>(["a", "b", "c"]);  
-const firstBoolean = getFirstElement<boolean>([true, false]);  
-
-console.log(firstNumber);   
-console.log(firstString);   
-console.log(firstBoolean);  
-
+getFirstElement<number>([1, 2, 3]); 
+getFirstElement<string>(["a", "b", "c"]);
+getFirstElement<boolean>([true, false, true]);
 
 
 
